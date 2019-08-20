882c0cf5ef4e6ea845c70140c62b1532afe92670
DraqueT <draquemail@gmail.com>
2019-08-20 16:56:33 -0700

    initial commit (commits for a bit after this will not have tickets as the kinks are worked out in Java 12)

---
+++ build_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "cleaning paths..."
+rm -rf target/mods
+rm -rf build
+mkdir target/mods
+
+echo "creating jmod based on jar built without dependencies..."
+jmod create --class-path target/PolyGlotLinA-3.0.jar --main-class org.darisadesigns.polyglotlina.PolyGlot target/mods/PolyGlot.jmod
+
+echo "creating runnable image..."
+jlink --module-path "/Users/draque/.m2/repository/org/apache/poi/poi-ooxml/4.1.0/poi-ooxml-4.1.0.jar:/Users/draque/.m2/repository/org/apache/poi/poi/4.1.0/:/Users/draque/.m2/repository/com/itextpdf/layout/7.0.4/:/Users/draque/.m2/repository/org/apache/commons/commons-text/1.7/:/Users/draque/.m2/repository/com/itextpdf/io/7.0.4/:/Users/draque/.m2/repository/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar:/Users/draque/.m2/repository/org/jsoup/jsoup/1.12.1/jsoup-1.12.1.jar:target/mods:/Users/draque/.m2/repository/org/openjfx/javafx-graphics/12.0.2/:/Users/draque/.m2/repository/org/openjfx/javafx-base/12.0.2/:/Users/draque/.m2/repository/org/openjfx/javafx-media/12.0.2/:/Users/draque/.m2/repository/org/openjfx/javafx-swing/12.0.2/:/Users/draque/.m2/repository/org/openjfx/javafx-controls/12.0.2/:/Users/draque/.sdkman/candidates/java/12.0.1.j9-adpt/jmods/" --add-modules "org.darisadesigns.polyglotlina.polyglot" --output "build/image/" --launcher PolyGlot=org.darisadesigns.polyglotlina.polyglot
+
+echo "Done!"
